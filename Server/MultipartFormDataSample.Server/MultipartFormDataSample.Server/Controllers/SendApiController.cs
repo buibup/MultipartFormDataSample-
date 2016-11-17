0ad1e337247c0354bd2a06c38974931f4e355c6a
Mongkol Meanrat <Mongkol Meanrat>
2016-11-18 06:04:01 +0700

    insert to database

--- Server/MultipartFormDataSample.Server/MultipartFormDataSample.Server/Controllers/SendApiController.cs
+++ Server/MultipartFormDataSample.Server/MultipartFormDataSample.Server/Controllers/SendApiController.cs
@@ -14,22 +14,46 @@ namespace MultipartFormDataSample.Server.Controllers
 {
     public class SendApiController : ApiController
     {
-        [Route("api/send")]
-        public string UploadImageSet(ImageSet model)
-        {
-            var sb = new StringBuilder();
+        //[Route("api/send")]
+        //public string UploadImageSet(ImageSet model)
+        //{
+        //    var sb = new StringBuilder();
             
-            sb.AppendFormat("Received image set {0}: ", model.Name);
-            model.Images.ForEach(i =>
-                sb.AppendFormat("Got image {0} of type {1} and size {2} bytes,", i.FileName, i.MimeType,
-                    i.ImageData.Length)
-                );
+        //    sb.AppendFormat("Received image set {0}: ", model.Name);
+        //    model.Images.ForEach(i =>
+        //        sb.AppendFormat("Got image {0} of type {1} and size {2} bytes,", i.FileName, i.MimeType,
+        //            i.ImageData.Length)
+        //        );
 
-            var result = sb.ToString();
-            Trace.Write(result);
+        //    var result = sb.ToString();
+        //    Trace.Write(result);
 
-            return result;
+        //    return result;
 
+        //}
+        [Route("api/send")]
+        public bool UpLoadImageSetToDb(ImageSet model)
+        {
+            bool flag = false;
+            string cmdString= "INSERT INTO Image(FileName, MimeType, ImageData) VALUES (@FileName, @MimeType, @ImageData)";
+            string conString = ConfigurationManager.ConnectionStrings["sql"].ToString();
+
+            foreach (var modelImage in model.Images)
+            {
+                using (SqlConnection connection = new SqlConnection(conString))
+                {
+                    SqlCommand cmd = new SqlCommand(cmdString);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = connection;
+                    cmd.Parameters.AddWithValue("@FileName", modelImage.FileName);
+                    cmd.Parameters.AddWithValue("@MimeType", modelImage.MimeType);
+                    cmd.Parameters.AddWithValue("@ImageData", modelImage.ImageData);
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            return flag;
         }
     }
 }
